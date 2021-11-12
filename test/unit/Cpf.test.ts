23617dd429b34a99dbb4f60477b30027ba90e7f8
Weliton H. Sernajotto <41920587+welitonhs@users.noreply.github.com>
2021-11-12 09:59:40 -0300

    Teste

--- test/unit/Cpf.test.ts
+++ test/unit/Cpf.test.ts
@@ -3,7 +3,7 @@ import { Cpf } from "../../src/domain/entity/Cpf"
 test("Deve validar um cpf", function(){
     const cpf = new Cpf("847.903.332-05");
     expect(cpf).toBeDefined();
-});
+});   
 
 test("Deve retornar um erro ao validar um cpf inválido", function(){
     expect(() => new Cpf("111.111.111-11")).toThrow(new Error("Invalid cpf"));
